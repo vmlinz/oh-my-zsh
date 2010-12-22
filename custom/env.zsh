2eb92c885ec8104e088b063fda47d55fa8207614
Zaicheng Qi <vmlinz@gmail.com>
2010-12-22 22:31:25 +0800

    more cusdomizations from .bashrc

--- custom/env.zsh
+++ custom/env.zsh
@@ -21,3 +21,25 @@ bindkey -e
 export GPGKEY=7527C999
 
 stty stop ^X
+
+# play with the go-lang
+export GOROOT=$HOME/Projects/go/go-lang
+export GOARCH=amd64
+export GOOS=linux
+
+# Android Sdk Tools
+if [ -d $HOME/Projects/android-sdk-linux_86/tools ]; then
+    ANDROID_SDK_DIR=/home/vmlinz/Projects/android-sdk-linux_86/tools
+else
+    ANDROID_SDK_DIR=
+    echo "Android SDK not found!"
+fi
+
+# Android ndk-r4
+if [ -d $HOME/Projects/android/ndks/android-ndk-r4 ]; then
+    ANDROID_NDK_DIR=$HOME/Projects/android/ndks/android-ndk-r4
+else
+    ANDROID_NDK_DIR=
+    echo "Android ndk-r4 not found!"
+fi
+export PATH=$ANDROID_NDK_DIR:$ANDROID_SDK_DIR:$PATH
