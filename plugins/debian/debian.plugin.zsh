49f055b466f7e047de267121daf10178bb6e7ea2
Zaicheng Qi <vmlinz@gmail.com>
2014-06-18 01:24:16 +0800

    * rename ag->au to leave ag to the silver seacher ag

--- plugins/debian/debian.plugin.zsh
+++ plugins/debian/debian.plugin.zsh
@@ -48,7 +48,7 @@ if [[ $use_sudo -eq 1 ]]; then
     alias adg='sudo $apt_pref update && sudo $apt_pref upgrade'
     alias adu='sudo $apt_pref update && sudo $apt_pref dist-upgrade'
     alias afu='sudo apt-file update'
-    alias ag='sudo $apt_pref upgrade'
+    alias au='sudo $apt_pref upgrade'
     alias ai='sudo $apt_pref install'
     # Install all packages given on the command line while using only the first word of each line:
     # acs ... | ail
@@ -83,7 +83,7 @@ else
     alias adg='su -lc \'$apt_pref update && aptitude safe-upgrade\' root'
     alias adu='su -lc \'$apt_pref update && aptitude dist-upgrade\' root'
     alias afu='su -lc "apt-file update"'
-    alias ag='su -lc \'$apt_pref safe-upgrade\' root'
+    alias au='su -lc \'$apt_pref safe-upgrade\' root'
     ai() {
         cmd="su -lc 'aptitude -P install $@' root"
         print "$cmd"
@@ -136,7 +136,7 @@ apt_pref_compdef abd "build-dep"
 apt_pref_compdef ac  "clean"
 apt_pref_compdef ad  "update"
 apt_pref_compdef afu "update"
-apt_pref_compdef ag  "upgrade"
+apt_pref_compdef au  "upgrade"
 apt_pref_compdef ai  "install"
 apt_pref_compdef ail "install"
 apt_pref_compdef ap  "purge"
